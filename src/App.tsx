f738af96247bb8f700152818d5a541db2db452d2
Viniciuslb <viniciuslb067@gmail.com>
2021-07-10 00:26:17 -0300

    feat: authContext and useAuth hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import ptBR from "antd/lib/locale/pt_BR";
+
+import { Home } from "./pages/Home";
+import { Panel } from "./pages/Panel";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <ConfigProvider locale={ptBR}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/control" component={Panel} />
+          </Switch>
+        </ConfigProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
